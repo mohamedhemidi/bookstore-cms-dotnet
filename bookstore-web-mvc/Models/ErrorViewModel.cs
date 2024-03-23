66afa35d7b27c70904cedab5bad5e8782f808138
Mohamed <m.hamidi571@gmail.com>
2024-03-23 18:44:24 +0100

    bookstore web mvc project init

---
+++ bookstore-web-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace bookstore_web_mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
